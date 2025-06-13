f618d6cc2b0b2da623089cc6a23cc71e69ea2215
Nabil <cleo.master0000@gmail.com>
2025-06-13 16:57:42 +0500

    Skip authentication from Chat endpoint for simplicity

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -6,14 +6,15 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { ChatService } from './chat.service';
-import { SkipAuth } from '../auth/decorators/skipauth.decorator';
 import { AuthGuard } from '../core/guards/auth.guard';
+import { SkipAuth } from '../auth/decorators/skipauth.decorator';
 
 @Controller('chat')
 @UseGuards(AuthGuard)
 export class ChatController {
   constructor(private readonly chatService: ChatService) {}
 
+  // We have skipped the auth
   @Get(':prompt')
   @SkipAuth()
   handlePrompt(@Param('prompt') prompt: string) {
